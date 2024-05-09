94d110755db91009efa61e8b0ae448e9489566b6
martinvonhradsky <xhradsky@stuba.sk>
2024-05-09 19:23:33 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-ping "$1";
 
-sleep(1000);
-return 0;
+# Run ping command in the background
+ping "$1" &
+
+# Sleep for 5 seconds (5000 milliseconds)
+sleep 5
+
+# After 5 seconds, kill the ping command
+kill $!
+
+# Return exit status 0
+exit 0
